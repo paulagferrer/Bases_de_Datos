6e86fb4ee5c26d66bac7edd93b9bf56b07305f88
Paula Gª Ferrer <paulagarciaferr329@gmail.com>
2026-03-05 12:25:49 +0100

    Acabant exercicis

--- Python_projects_BaseDatos/consultas_sin_agregacion.py
+++ Python_projects_BaseDatos/consultas_sin_agregacion.py
@@ -8,9 +8,10 @@ cnx = mysql.connector.connect(
         database="airportdb"
     )
 cur = cnx.cursor()
-
+# exercicis semana 3_2
 EJERCICIO_27 = False
-EJERCICIO_34 = True
+EJERCICIO_34 = False
+EJERCICIO_40 = True
 
 if EJERCICIO_27:
     cur.execute("SELECT * FROM airport_geo")
@@ -33,17 +34,44 @@ if EJERCICIO_27:
     print("País con más aeropuertos:", max_country)
 
 if EJERCICIO_34:
-    # ejerdicio 34
     cur.execute("SELECT flightno, departure FROM flight;")
     rows = cur.fetchall()
     cols = [d[0] for d in cur.description]
     df_flights = pd.DataFrame(rows, columns=cols)
 
-    # ejercicio 36
+    print("Ejercicio 34:")
+    print(df_flights.head())
+
     df_flights['departure'] = pd.to_datetime(df_flights['departure'])
+    print("Ejercicio 36:")
+    print(df_flights.dtypes)
 
-    # ejericio 37 extraer año
     df_flights['year'] = df_flights['departure'].dt.year
+    print("Ejercicio 37:")
+    print(df_flights.head())
 
-    #ejericio 38 ordenar por fecha de salida descendente
     df_flights = df_flights.sort_values('departure', ascending=False)
+    print("Ejercicio 38:")
+    print(df_flights.head())
+
+if EJERCICIO_40:
+    cur.execute("""
+                SELECT *
+                FROM flight
+                WHERE departure > %s
+                """, ("2015-01-07",))
+
+    rows = cur.fetchall()
+    columnas = [col[0] for col in cur.description]
+
+    df_new = pd.DataFrame(rows, columns=columnas) #ecercici 41
+
+    conteo_salida = {} #exericci 42
+    for _, row in df_new.iterrows():
+        aeropuerto = row['from']
+        conteo_salida[aeropuerto] = conteo_salida.get(aeropuerto, 0) + 1
+
+    max_salida = max(conteo_salida.items(), key=lambda x: x[1])
+    print("Ejercicio 42:")
+    print(max_salida)
+
